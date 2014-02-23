23e853f0b055ac1f98a7e8423597c3802da0eaf9
Kyle <kylegluce@gmail.com>
2014-02-23 14:26:21 -0800

    added messages

--- twitch_split/splitter/lib.py
+++ twitch_split/splitter/lib.py
@@ -2,7 +2,17 @@ import requests
 
 def validate_streams(param_usernames):
 	streams = {}
+
 	for username in param_usernames:
 		r = requests.get('https://api.twitch.tv/kraken/streams/' + username, headers={"Accept": "application/vnd.twitchtv.v3+json"})
 		streams[username] = r.json()
-	return streams
+
+	for key in streams:
+
+		try:
+			error = streams[key][u'error']
+			return False
+		except KeyError:
+			pass
+
+	return True
--- twitch_split/splitter/views.py
+++ twitch_split/splitter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from splitter import lib
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,9 +15,16 @@ def one_full(request, param_username):
 def two_horiz(request, param_username_1, param_username_2):
 	entered_streams = [param_username_1, param_username_2]
 	page_vars = { 'page_title': 'Double View' }
-
-	streams = lib.validate_streams(entered_streams)
-	page_vars['layout'] = "two_horiz"
 	page_vars['streams'] = entered_streams
 
-	return render(request, "two_horiz.html", page_vars)
+	if lib.validate_streams(entered_streams) == False:
+		messages.add_message(request, messages.ERROR, 'ERROR: Invalid STREAM')
+
+	orientation = request.GET.get('orientation', 'horizontal')
+
+	if orientation == 'vertical':
+		page_vars['layout'] = 'two_vert'
+		return render(request, 'two_vert.html', page_vars)
+	else:
+		page_vars['layout'] = 'two_horiz'
+		return render(request, 'two_horiz.html', page_vars)
